287d60e1069a414e97b8d61c84e622c77955b4c6
Stefan <stefan.goerig@gmx.de>
2024-09-21 12:18:01 +0200

    Forms zum Bearbeiten zugefügt

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,7 @@
-from django.shortcuts import render
 from django.utils import timezone
+from django.shortcuts import redirect, render, get_object_or_404
 from .models import Post
 from .forms import PostForm
-from django.shortcuts import redirect
-from django.shortcuts import render, get_object_or_404
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
