6fe49f554e60145573840b9be101cfbd88a833f3
Stefan <stefan.goerig@gmx.de>
2024-09-22 09:06:14 +0200

    Kommentarfunktion hinzugefügt

--- blog/admin.py
+++ blog/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Comment
 
-admin.site.register(Post)
+admin.site.register(Post)       # Lasse die Admin-App Posts verwalten
+admin.site.register(Comment)    # Lasse die Admin-App Kommentare verwalten
